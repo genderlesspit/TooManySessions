69568fe3f5bdfda289c25a9e5f1c7dd0620b93e1
genderlesspit <59433707+genderlesspit@users.noreply.github.com>
2025-07-26 13:57:33 -0500

    0.1.9 - cfg kwargs

--- src/toomanysessions/__init__.py
+++ src/toomanysessions/__init__.py
@@ -7,4 +7,5 @@ CWD_TEMPLATER = Environment(loader=FileSystemLoader(Path(__file__).parent))
 from .sessions import Session, Sessions, authenticate
 from .users import User, Users
 from .core import SessionedServer
-
+from .msft_oauth import MicrosoftOAuth
+from .msft_graph_api import GraphAPI
